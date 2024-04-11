bdcf7ebe0e65a5f167efd2903b80018556d91bc7
Marc <marc@zephir.ch>
2024-04-11 11:36:41 +0000

    Add docker dev env; Update Readme #5

---
+++ docker/snippets/footer.php
@@ -0,0 +1,53 @@
+<?php
+/*
+  Snippets are a great way to store code snippets for reuse
+  or to keep your templates clean.
+
+  This footer snippet is reused in all templates.
+
+  More about snippets:
+  https://getkirby.com/docs/guide/templates/snippets
+*/
+?>
+  </main>
+
+  <footer class="footer">
+    <div class="grid">
+      <div class="column" style="--columns: 8">
+        <h2><a href="https://getkirby.com">Made with Kirby</a></h2>
+        <p>
+          Kirby: the file-based CMS that adapts to any project, loved by developers and editors alike
+        </p>
+      </div>
+      <div class="column" style="--columns: 2">
+        <h2>Pages</h2>
+        <ul>
+          <?php foreach ($site->children()->listed() as $example): ?>
+          <li><a href="<?= $example->url() ?>"><?= $example->title()->esc() ?></a></li>
+          <?php endforeach ?>
+        </ul>
+      </div>
+      <div class="column" style="--columns: 2">
+        <h2>Kirby</h2>
+        <ul>
+          <li><a href="https://getkirby.com">Website</a></li>
+          <li><a href="https://getkirby.com/docs">Docs</a></li>
+          <li><a href="https://forum.getkirby.com">Forum</a></li>
+          <li><a href="https://chat.getkirby.com">Chat</a></li>
+          <li><a href="https://github.com/getkirby">GitHub</a></li>
+        </ul>
+      </div>
+    </div>
+  </footer>
+
+  <?= js([
+    'assets/js/prism.js',
+    'assets/js/lightbox.js',
+    'assets/js/index.js',
+    '@auto'
+  ]) ?>
+
+  <?= snippet('cookieconsentJs') ?>
+
+</body>
+</html>
