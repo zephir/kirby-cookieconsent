04601efc1e02573fa067b1a4f6b4c19b7081bfc8
Daniel Freudenberg <freudend@gmail.com>
2024-05-04 02:11:12 +0200

    Added dutch translations

---
+++ translations/nl.php
@@ -0,0 +1,49 @@
+<?php
+
+return [
+    "consentModal" => [
+        "title" => "🍪&nbsp;&nbsp;Cookies",
+        "description" => "We gebruiken cookies om ervoor te zorgen dat je de beste ervaring hebt op onze website.",
+        "acceptAllBtn" => "Alles accepteren",
+        "acceptNecessaryBtn" => "Alles afwijzen",
+        "showPreferencesBtn" => "Instellingen"
+    ],
+    "preferencesModal" => [
+        "title" => "🍪&nbsp;&nbsp;Cookie voorkeuren",
+        "acceptAllBtn" => "Accepteer alles",
+        "acceptNecessaryBtn" => "Weiger alles",
+        "savePreferencesBtn" => "Instellingen opslaan",
+        "closeIconLabel" => "Sluit",
+        "sections" => [
+            [
+                "title" => "Cookie Usage",
+                "description" => "We gebruiken cookies om ervoor te zorgen dat je de beste ervaring hebt op onze website."
+            ],
+            [
+                "title" => "Noodzakelijk",
+                "description" => "Deze cookies worden gebruikt voor activiteiten die strikt noodzakelijk zijn voor de werking of levering van de dienst die jij bij ons hebt aangevraagd.",
+                "linkedCategory" => "necessary"
+            ],
+            [
+                "title" => "Functionaliteit",
+                "description" => "These cookies enable basic interactions and functionalities that allow you to access selected features of our service and facilitate your communication with us.",
+                "linkedCategory" => "functionality",
+            ],
+            [
+                "title" => "Ervaring",
+                "description" => "Deze cookies helpen ons om de kwaliteit van uw gebruikerservaring te verbeteren en interacties met externe inhoud, netwerken en platforms mogelijk te maken.",
+                "linkedCategory" => "experience",
+            ],
+            [
+                "title" => "Statistieken",
+                "description" => "Deze cookies helpen ons het verkeer te meten en uw gedrag te analyseren om onze service te verbeteren.",
+                "linkedCategory" => "measurement",
+            ],
+            [
+                "title" => "Marketing",
+                "description" => "Deze cookies helpen ons om u gepersonaliseerde advertenties of marketingcontent te leveren en om de prestaties ervan te meten.",
+                "linkedCategory" => "marketing",
+            ]
+        ]
+    ],
+];
