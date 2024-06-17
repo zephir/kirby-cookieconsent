6e4cb71ed21d6a593cbc9a21249a4d07b91efa80
André Pimpão <a.pimpao@programmator.dev>
2024-06-17 11:50:26 +0100

    feat: added pt_PT translation

---
+++ translations/pt_PT.php
@@ -0,0 +1,49 @@
+<?php
+
+return [
+    "consentModal" => [
+        "title" => "🍪&nbsp;&nbsp;Cookies",
+        "description" => "Utilizamos cookies para garantir que tem a melhor experiência no nosso website.",
+        "acceptAllBtn" => "Aceitar todos",
+        "acceptNecessaryBtn" => "Rejeitar todos",
+        "showPreferencesBtn" => "Configurações"
+    ],
+    "preferencesModal" => [
+        "title" => "🍪&nbsp;&nbsp;Preferências de Cookies",
+        "acceptAllBtn" => "Aceitar todos",
+        "acceptNecessaryBtn" => "Rejeitar todos",
+        "savePreferencesBtn" => "Guardar configurações",
+        "closeIconLabel" => "Fechar",
+        "sections" => [
+            [
+                "title" => "Uso de Cookies",
+                "description" => "Utilizamos cookies para garantir que tem a melhor experiência no nosso website."
+            ],
+            [
+                "title" => "Necessários",
+                "description" => "Estes cookies são utilizados para atividades estritamente necessárias para operar ou fornecer o serviço que solicitou.",
+                "linkedCategory" => "necessary"
+            ],
+            [
+                "title" => "Funcionalidade",
+                "description" => "Estes cookies permitem interações e funcionalidades básicas que permitem aceder a funcionalidades selecionadas do nosso serviço e facilitam a sua comunicação connosco.",
+                "linkedCategory" => "functionality",
+            ],
+            [
+                "title" => "Experiência",
+                "description" => "Estes cookies ajudam a melhorar a qualidade da sua experiência de utilizador e a habilitar interações com conteúdo externo, redes e plataformas.",
+                "linkedCategory" => "experience",
+            ],
+            [
+                "title" => "Medição",
+                "description" => "Estes cookies ajudam a medir o tráfego e a analisar o seu comportamento para melhorar o nosso serviço.",
+                "linkedCategory" => "measurement",
+            ],
+            [
+                "title" => "Marketing",
+                "description" => "Estes cookies ajudam a fornecer anúncios personalizados ou conteúdo de marketing para si, e a medir o seu desempenho.",
+                "linkedCategory" => "marketing",
+            ]
+        ]
+    ],
+];
